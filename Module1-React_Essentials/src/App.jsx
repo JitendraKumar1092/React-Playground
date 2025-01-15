a811088f832b97dccee34435204d8c6f47153b76
JitendraKumar1092 <egr.jitendrayadav@gmail.com>
2025-01-16 02:12:25 +0530

    Module 1 - Essentials

--- Module1-React_Essentials/src/App.jsx
+++ Module1-React_Essentials/src/App.jsx
@@ -1,28 +1,35 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import TabButton from "./components/TabButton";
+
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
 
-function Header() {
-  return (
-    <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        Fundamental React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
 function App() {
+  const [dynamicContent, setDynamicContent] = useState("");
+  function handleClick(name) {
+    setDynamicContent(name);
+  }
   return (
     <div>
       <Header />
       <main>
         <h2>Time to get started!</h2>
+        <section id="example">
+          <h2>Example</h2>
+          <menu>
+            <TabButton onSelect={() => handleClick("Components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleClick("JSX")}>JSX</TabButton>
+            <TabButton onSelect={() => handleClick("Props")}>Props</TabButton>
+            <TabButton onSelect={() => handleClick("State")}>State</TabButton>
+          </menu>
+          {dynamicContent && <div>{dynamicContent}</div>}
+        </section>
       </main>
     </div>
   );
